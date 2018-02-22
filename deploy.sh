9b811eeb7b5f421cc6d380bba54279ae48d815b7
Ali Arshad <ali.arshad@crossover.com>
2018-02-23 00:50:59 +0500

    Added docker-compose

--- deploy.sh
+++ deploy.sh
@@ -32,3 +32,13 @@ apt-get update
 # Install Docker CE
 apt-get install -y docker-ce
 
+# get latest docker compose released tag
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+# Install docker-compose
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+chmod +x /usr/local/bin/docker-compose
+
+# Show docker-compose version
+docker-compose --version
+
