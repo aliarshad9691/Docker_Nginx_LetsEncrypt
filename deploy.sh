06c4d8e99f1613d609310d174622d4cc182798b3
aliarshad9691 <email@aliarshad.info>
2018-02-24 14:10:41 +0500

    Added some validations

--- deploy.sh
+++ deploy.sh
@@ -1,62 +1,72 @@
 #!/usr/bin/env bash
 
-# Update apt-get
-apt-get update
+command -v docker >/dev/null 2>&1 || {
+    echo "Docker not found. Installing..." >&2;
+    # Update apt-get
+    apt-get update
 
-# Lets remove any old Docker installations.
-apt-get remove -y docker docker-engine docker-ce docker.io
+    # Lets remove any old Docker installations.
+    apt-get remove -y docker docker-engine docker-ce docker.io
 
-# Install Docker dependencies & git.
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common \
-    git
+    # Install Docker dependencies & git.
+    apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common \
+        git
 
-# Adding Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    # Adding Docker’s official GPG key
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# Verify fingerprint
-apt-key fingerprint 0EBFCD88
+    # Verify fingerprint
+    apt-key fingerprint 0EBFCD88
 
-# Adding repository
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+    # Adding repository
+    add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
 
-# Update apt-get
-apt-get update
+    # Update apt-get
+    apt-get update
 
-# Install Docker CE
-apt-get install -y docker-ce
+    # Install Docker CE
+    apt-get install -y docker-ce
+}
 
-# get latest docker compose released tag
-COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+command -v docker-compose >/dev/null 2>&1 || {
+    echo "docker-compose not found. Installing..." >&2;
+    # get latest docker compose released tag
+    COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
-# Install docker-compose
-sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-chmod +x /usr/local/bin/docker-compose
+    # Install docker-compose
+    sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+    chmod +x /usr/local/bin/docker-compose
 
-# Show docker-compose version
-docker-compose --version
+    # Show docker-compose version
+    docker-compose --version
+    }
 
 
-# Adding certbot repo
-add-apt-repository ppa:certbot/certbot -y
+    command -v certbot >/dev/null 2>&1 || {
+    # Adding certbot repo
+    add-apt-repository ppa:certbot/certbot -y
 
-# Update apt-get
-apt-get update
+    # Update apt-get
+    apt-get update
 
-# Install certbot
-apt-get install -y certbot python-certbot-apache
+    # Install certbot
+    apt-get install -y certbot python-certbot-apache
+}
 
-# Install Nginx
-apt-get install -y nginx
 
-# Adding reverse proxy generic settings
-/bin/cat <<EOM > /etc/nginx/reverse-proxy.conf
+command -v nginx >/dev/null 2>&1 || {
+    # Install Nginx
+    apt-get install -y nginx
+
+    # Adding reverse proxy generic settings
+    /bin/cat <<EOM > /etc/nginx/reverse-proxy.conf
 proxy_pass_header Server;
 proxy_set_header Host \$http_host;
 proxy_redirect off;
@@ -70,8 +80,15 @@ proxy_set_header Connection "";
 proxy_http_version 1.1;
 EOM
 
-#creating certificates directory
-mkdir -p /etc/nginx/certificates/
+    #creating certificates directory
+    mkdir -p /etc/nginx/certificates/
+
+}
+
+
+
+
+
 
 # creating certbot dir for domain
 mkdir -p /var/www/${1}
@@ -92,13 +109,14 @@ server {
 EOM
 
 # Reloading Nnginx
-service nginx reload
+service nginx restart
 
 # Adding SSL
 rm /etc/nginx/certificates/${1}/cert.pem >/dev/null 2>&1
 rm /etc/nginx/certificates/${1}/key.pem >/dev/null 2>&1
 rm -rf /etc/letsencrypt/live/${1} >/dev/null 2>&1
 rm -rf /etc/letsencrypt/renewal/${1}.conf >/dev/null 2>&1
+rm -rf /etc/letsencrypt/archive/${1} >/dev/null 2>&1
 
 certbot certonly --webroot -w /var/www/${1}/ -d ${1} --email ${3} --agree-tos --no-eff-email
 ln  -s /etc/letsencrypt/live/${1}/fullchain.pem /etc/nginx/certificates/${1}/cert.pem
@@ -153,6 +171,6 @@ server {
 EOM
 
 # Reloading Nnginx
-service nginx reload
+service nginx restart
 
 echo "Configured $1 to post: $2"
